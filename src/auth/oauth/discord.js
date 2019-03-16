dab49fcec941760cbe27c5ed09dde069f6e25915
Karl Polintan <kf.polntan@gmail.com>
2019-03-16 16:18:58 -0700

    converted hopefully most of the Google code to Discord

---
+++ src/auth/oauth/discord.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const superagent = require('superagent');
+const Users = require('../users-model.js');
+
+const API = 'http://localhost:3000';
+const discordAuth = 'https://discordapp.com/api/oauth2/authorize';
+const SERVICE = 'https://discordapp.com/api/oauth2/token';
+
+let authorize = (request) => {
+
+    console.log('(1)', request.query.code);
+
+    return superagent.post(discordAuth)
+        .type('form')
+        .send({
+            code: request.query.code,
+            client_id: process.env.DISCORD_CLIENT_ID,
+            client_secret: process.env.DISCORD_CLIENT_SECRET,
+            redirect_uri: `${API}/oauth`,
+            grant_type: 'authorization_code',
+        })
+        .then( response => {
+            let access_token = response.body.access_token;
+            console.log('(2)', access_token);
+            return access_token;
+        })
+        .then(token => {
+            console.log(SERVICE, token);
+            return superagent.get(SERVICE)
+                .set('Authorization', `Bearer ${token}`)
+                .then( response => {
+                    let user = response.body;
+                    console.log('(3)', user);
+                    return user;
+                });
+        })
+        .then( oauthUser => {
+            console.log('(4) Create Our Account');
+            return Users.createFromOauth(oauthUser.email);
+        })
+        .then( actualUser => {
+            return actualUser.generateToken();
+        })
+        .catch( error => error );
+};
+
+
+module.exports = authorize;
